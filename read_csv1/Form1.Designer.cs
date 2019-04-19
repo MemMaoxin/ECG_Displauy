2fb8886a759263a8b1d48c95e009a018765b4a15
MemMaoxin <272097277@qq.com>
2019-04-19 16:55:40 +0800

    添加项目文件。

---
+++ read_csv1/Form1.Designer.cs
@@ -0,0 +1,219 @@
+﻿namespace read_csv1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.button1 = new System.Windows.Forms.Button();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.Y_Range = new System.Windows.Forms.TextBox();
+            this.Com_ECG = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label_T = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(1037, 56);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(117, 34);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "打开心电串口";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // chart1
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(133, 79);
+            this.chart1.Name = "chart1";
+            series1.ChartArea = "ChartArea1";
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            this.chart1.Series.Add(series1);
+            this.chart1.Size = new System.Drawing.Size(1003, 312);
+            this.chart1.TabIndex = 3;
+            this.chart1.Text = "chart1";
+            // 
+            // Y_Range
+            // 
+            this.Y_Range.Location = new System.Drawing.Point(5, 109);
+            this.Y_Range.Name = "Y_Range";
+            this.Y_Range.Size = new System.Drawing.Size(109, 25);
+            this.Y_Range.TabIndex = 4;
+            this.Y_Range.Validated += new System.EventHandler(this.TextBox2_Validated);
+            // 
+            // Com_ECG
+            // 
+            this.Com_ECG.AcceptsReturn = true;
+            this.Com_ECG.Location = new System.Drawing.Point(889, 32);
+            this.Com_ECG.Name = "Com_ECG";
+            this.Com_ECG.Size = new System.Drawing.Size(100, 25);
+            this.Com_ECG.TabIndex = 5;
+            this.Com_ECG.Text = "com10";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(5, 79);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(105, 15);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "调整Y坐标范围";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.textBox3);
+            this.groupBox1.Controls.Add(this.textBox2);
+            this.groupBox1.Controls.Add(this.Com_ECG);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.chart1);
+            this.groupBox1.Controls.Add(this.Y_Range);
+            this.groupBox1.Location = new System.Drawing.Point(18, 31);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(2);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(2);
+            this.groupBox1.Size = new System.Drawing.Size(1197, 430);
+            this.groupBox1.TabIndex = 7;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "ECG";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(25, 268);
+            this.textBox3.Margin = new System.Windows.Forms.Padding(2);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(68, 25);
+            this.textBox3.TabIndex = 10;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(25, 208);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(2);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(68, 25);
+            this.textBox2.TabIndex = 9;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(518, 95);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(233, 25);
+            this.textBox1.TabIndex = 7;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.textBox4);
+            this.groupBox2.Controls.Add(this.textBox1);
+            this.groupBox2.Controls.Add(this.button2);
+            this.groupBox2.Controls.Add(this.label_T);
+            this.groupBox2.Location = new System.Drawing.Point(18, 475);
+            this.groupBox2.Margin = new System.Windows.Forms.Padding(2);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Padding = new System.Windows.Forms.Padding(2);
+            this.groupBox2.Size = new System.Drawing.Size(1197, 191);
+            this.groupBox2.TabIndex = 8;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "体表温度";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(776, 95);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(233, 25);
+            this.textBox4.TabIndex = 10;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(1019, 16);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(117, 31);
+            this.button2.TabIndex = 9;
+            this.button2.Text = "打开温度串口";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // label_T
+            // 
+            this.label_T.AutoSize = true;
+            this.label_T.Location = new System.Drawing.Point(302, 95);
+            this.label_T.Name = "label_T";
+            this.label_T.Size = new System.Drawing.Size(15, 15);
+            this.label_T.TabIndex = 7;
+            this.label_T.Text = "T";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1235, 659);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.TextBox Y_Range;
+        private System.Windows.Forms.TextBox Com_ECG;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label label_T;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox4;
+    }
+}
+
---
+++ read_csv1/Form1.cs
@@ -0,0 +1,369 @@
+﻿using System;
+using System.Windows.Forms;
+using System.IO.Ports;
+using System.Timers;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms.DataVisualization.Charting;
+using System.Collections.Concurrent;
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace read_csv1
+{
+
+    public partial class Form1 : Form
+    {
+        SerialPort port_xx;
+        SerialPort port_T;
+        public byte cs;
+        int rawdata = 0;
+        double raw = 0;
+        int index = 0;
+        int[] buffer_x = new int[6] { 70000, 70000, 70000, 70000, 70000, 70000 };
+        int[] buffer_y = new int[6] { 70000, 70000, 70000, 70000, 70000, 70000 };
+        int buffer_control = 0;
+
+        StringBuilder stringBuilder = new StringBuilder(2048);
+        BlockingCollection<int> dataItems = new BlockingCollection<int>(500);
+        private readonly Mutex mstringBuilderMutex = new Mutex();
+        System.Timers.Timer readBufferTimer = new System.Timers.Timer(1000);
+
+        public Form1()
+        {
+            InitializeComponent();
+            InitializeGraphs(chart1, SeriesChartType.FastLine, -0.5, 0.8);
+
+
+        }
+        private void InitializeGraphs(Chart chart, SeriesChartType type, double yMin, double yMax)
+        {
+            chart.Legends.Clear();
+
+            var series = chart.Series[0];
+            if (chart.Series.Count > 1)
+                chart.Series[1].ChartType = SeriesChartType.Point;
+            var chartArea = chart.ChartAreas[0];
+
+            series.ChartType = type;
+
+
+            series.XValueType = ChartValueType.Int32;
+
+            chartArea.AxisX.ScrollBar.Enabled = true;
+            chartArea.AxisX.ScrollBar.IsPositionedInside = false;
+
+            chartArea.AxisY.ScaleView.Size = 20;
+            chartArea.AxisX.ScaleView.Size = 2000;
+            //chartArea.AxisX.MajorTickMark.Interval = 1250;
+            //chartArea.AxisX.MajorGrid.Interval = 1250;
+            chartArea.CursorX.AutoScroll = true;
+            chartArea.CursorX.IsUserEnabled = true;
+            chartArea.CursorX.IsUserSelectionEnabled = true;
+            chartArea.AxisX.ScaleView.Zoomable = true;
+
+            chartArea.AxisX.Minimum = 0;
+
+            //chartArea.AxisY.Minimum = yMin;
+            //chartArea.AxisY.Maximum = yMax;
+        }
+        private void Change_Yscale(Chart chart, int Yscale)
+        {
+            var chartArea = chart.ChartAreas[0];
+            chartArea.AxisY.ScaleView.Size = Yscale;
+        }
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        public delegate void SetTextDeleg(String s);
+        public delegate void SetDlgDeleg(int x, double y);
+        public delegate void ClearDlgDeleg();
+        public void show_data(byte[] data, int bytes_number)
+        {
+
+            textBox1.AppendText(bytes_number.ToString() + Environment.NewLine);
+            //textBox1.AppendText((BitConverter.ToString(data)) + Environment.NewLine);
+        }
+
+        int check = 0, i = 0, mark = 0;
+        byte[] payload = new byte[4];
+        byte paylength;
+
+        int k = 0;
+        byte[] buffer_T = new byte[8];
+
+        void xx_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+
+            if (!port_xx.IsOpen) return;
+            //int bytes_number = port_xx.BytesToRead;
+
+            int bytes_number = 0;
+            if (port_xx.BytesToRead >= 64)
+                bytes_number = 64;
+            byte[] buffer = new byte[bytes_number];
+            port_xx.Read(buffer, 0, bytes_number);
+
+
+            int j = 0;
+
+            while (bytes_number > 0)
+            {
+                bytes_number--;
+                // textBox1.AppendText(bytes_number.ToString() + Environment.NewLine);
+                byte temp = buffer[j++];
+
+                if (temp == 0xBB && k == 0)
+                {
+                    buffer_T[0] = temp;
+                    k++;
+                }
+                else if (k != 0 && k < 8)
+                    buffer_T[k++] = temp;
+                else if (k == 8)
+                {
+
+                    int p = 0;
+                    while (buffer_T[p] == 0xBB)
+                    {
+                        p++;
+                    }
+                    if (p == 6)
+                    {
+                        byte[] Payload_T = new byte[2];
+                        string T_str;
+                        double Tnow = 0;
+                        int radio = 0;
+                        Payload_T[0] = buffer_T[6];
+                        Payload_T[1] = buffer_T[7];
+                        radio = (Payload_T[0] << 8) | Payload_T[1];
+                        int Rref = 158;
+                        double R = (double)radio * Rref / (65535 - radio);
+                        double R0 = 159;
+                        double T0 = 20;
+                        double change_radio = 0.5;
+                        Tnow = T0 + (R - R0) / change_radio;
+                        T_str = Tnow.ToString() + "     ";
+                        BeginInvoke(new SetTextDeleg((String str) =>
+                        {
+                            textBox1.Text = Tnow.ToString("0.00");
+                            //textBox1.Text = buffer[0].ToString();
+                            //textBox4.Text = buffer[1].ToString();
+                        }), new object[] { T_str });
+                    }
+                    k = 0;
+                }
+
+                if (check == 0 && temp == 0xaa)
+                {
+                    check = 1;
+                    continue;
+                }
+                if (check == 1)
+                {
+                    if (temp == 0xaa)
+                    {
+                        check = 2;
+                        continue;
+                    }
+                    else
+                    {
+                        check = 0;
+                        continue;
+                    }
+                }
+                if (check == 2)
+                {
+                    paylength = temp;
+                    if (paylength == 0x04)
+                    {
+                        check = 3;
+                        continue;
+                    }
+                    else
+                    {
+                        check = 0;
+                        continue;
+                    }
+                }
+                if (check >= 3)
+                {
+                    if (i < 4)
+                    {
+                        payload[i++] = temp;
+                    }
+                    else
+                    {
+                        cs = temp;
+                        i = 0; check = 0; mark = 1;
+                    }
+                }
+
+                if (i == 0 && check == 0 && mark == 1)
+                {
+                    int bytecount = 0;
+                    byte checksum = 0x00;
+                    byte code, length;
+
+                    for (int k = 0; k < 4; k++)
+                        checksum += payload[k];
+
+                    checksum = (byte)(~(int)(checksum & 0xFF));
+                    if (checksum != cs)
+                        continue;
+                    else
+                    {
+                        code = payload[bytecount++];
+                        if (((int)code & 0x80) != 0) length = payload[bytecount++];
+                        else length = 1;
+
+                        rawdata = (payload[bytecount] << 8 | payload[bytecount + 1]);
+                        if (rawdata >= 32768) rawdata = rawdata - 65536;
+
+
+                        buffer_x[buffer_control] = rawdata;
+
+                        if ((buffer_x[(buffer_control + 1) % 6] != 70000))
+                        {
+                            buffer_y[buffer_control] = buffer_y[(buffer_control + 5) % 6] + (buffer_x[buffer_control] - buffer_x[(buffer_control + 1) % 6]) / 5;
+                        }
+                        else
+                        {
+                            buffer_y[buffer_control] = buffer_x[buffer_control];
+                        }
+
+
+
+                        // raw = rawdata * 18.3 / 128.0;
+
+                        if (!dataItems.IsAddingCompleted)
+                            dataItems.Add(rawdata);
+                            //dataItems.Add(buffer_y[buffer_control]);
+                        buffer_control = (buffer_control + 1) % 6;
+                    }
+                    mark = 0;
+                }
+            }
+        }
+
+
+
+        public void DataChart(TaskScheduler uiScheduler)
+        {
+            foreach (var data in dataItems.GetConsumingEnumerable())
+            //               BeginInvoke(new SetTextDeleg((String str) => {
+            //                 textBox1.AppendText(str);
+            //             }), new object[] { index +"\n" });
+            //         index++;
+            {
+                if ((index++) % 2000 == 0)
+                    BeginInvoke(new ClearDlgDeleg(() =>
+                    {
+                        chart1.Series[0].Points.Clear();
+                    }), new object[] { });
+                //double data_show = data * 18.3 / 51200.0;
+                double data_show = data / 3000.0;
+                BeginInvoke(new SetDlgDeleg((int x, double y) =>
+                {
+                    chart1.Series[0].Points.AddXY(x, y);
+                }), new object[] { (index) % 2000, data_show });
+            }
+        }
+        private void textBox2_TextChanged(object sender, EventArgs e) { }
+        private void TextBox2_Validated(object sender, EventArgs e)
+        {
+
+            if (!string.IsNullOrEmpty(Y_Range.Text))
+            {
+                //textBox1.AppendText("invalid textbox value, could not be null");
+                try
+                {
+                    int A = int.Parse(Y_Range.Text);
+                    Change_Yscale(chart1, A);
+                    textBox1.AppendText("ok\n");
+                }
+                catch
+                {
+                    textBox1.AppendText("invalid textbox value\n");
+                }
+
+            }
+
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string com_number = Com_ECG.Text;
+            port_xx = new SerialPort(com_number, 57600);
+            TaskScheduler uiScheduler = TaskScheduler.FromCurrentSynchronizationContext();
+            Task.Factory.StartNew(() => DataChart(uiScheduler));
+            try
+            {
+                port_xx.Open();
+                port_xx.DataReceived += xx_DataReceived;
+
+            }
+            catch
+            {
+                label_T.Text = "No" + com_number;
+            }
+
+        }
+
+        void xx_DataReceived_T(object sender, SerialDataReceivedEventArgs e)
+        {
+            if (!port_T.IsOpen) return;
+            int bytes_number = port_T.BytesToRead;
+            //int bytes_number = 2;
+            byte[] buffer = new byte[bytes_number];
+
+            string T_str;
+            double Tnow=0;
+            int radio = 0;
+            if (bytes_number >= 2)
+            {
+                port_T.Read(buffer, 0, bytes_number);
+                radio = (buffer[0] << 8) | buffer[1];
+                int Rref = 158;
+                double R = (double)radio * Rref / (65535 - radio);
+                double R0 = 159;
+                double T0 = 20;
+                double change_radio = 0.5;
+                Tnow = T0 + (R - R0) / change_radio;
+                T_str = Tnow.ToString() + "     ";
+                BeginInvoke(new SetTextDeleg((String str) =>
+                {
+                    textBox1.Text = Tnow.ToString("0.00");
+                    //textBox1.Text = buffer[0].ToString();
+                    //textBox4.Text = buffer[1].ToString();
+                }), new object[] { T_str });
+            }
+
+
+
+        }
+        /*
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string com_number = Com_T.Text;
+            port_T = new SerialPort(com_number, 57600);
+
+            try
+            {
+                port_T.Open();
+                port_T.DataReceived += xx_DataReceived_T;
+
+            }
+            catch
+            {
+                label_T.Text = "No" + com_number;
+            }
+        }
+        */
+
+    }
+}
